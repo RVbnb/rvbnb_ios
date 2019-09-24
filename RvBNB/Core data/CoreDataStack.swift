94b2842d75c82d078d195f02db0de7e06edc91eb
bfvilchez <bfvilchez@icloud.com>
2019-09-24 09:39:25 -0600

    created a core data stack alongside a data model

---
+++ RvBNB/Core data/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataStack.swift
+//  RvBNB
+//
+//  Created by brian vilchez on 9/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import  CoreData
+
+extension NSManagedObjectContext {
+    func saveChanges() {
+        if hasChanges {
+            do {
+                try save()
+            } catch {
+                fatalError("error saving to persistent store: \(error)")
+            }
+        }
+    }
+}
+
+
+class CoreDataStack {
+    
+    lazy var persistentContainer:NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RvBNB")
+        container.loadPersistentStores { (_, error) in
+            if let error = error as NSError? {
+                fatalError("error loading from persistence store: \(error)")
+            }
+            
+        }
+        return container
+    }()
+    
+    lazy var mainContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
+}
